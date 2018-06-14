956e25e8104cdce7291d763fe176ca5e2f4f786f
PhamSinh <sinhpv86@gmail.com>
2018-06-14 16:21:36 +0700

    update function create and function show

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -4,7 +4,6 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
 module.exports = {
 
 
@@ -13,19 +12,45 @@ module.exports = {
    * @param req
    * @param res
    */
-    'new' : function (req, res) {
-        res.view();
-    },
+  'new': function (req, res) {
+    res.view();
+  },
+  /**
+   * create customer
+   * @param req
+   * @param res
+   */
 
+  create: async function (req, res) {
 
-    create : function (req, res, next) {
-        Customer.create(req.params.all(), function customerCreated(err, customer) {
-          if(err) {
-            return next(err);
-          }
-
-          res.redirect('/customer/show/' + customer.id )
-        })
+    var params = _.extend(req.query || {}, req.params || {}, req.body || {});
+    var createdUser = await Customer.create(params).fetch();
+    if (!createdUser) {
+      res.send('Create error', 500);
     }
+    res.redirect('/customer/show/' + createdUser.id);
+    // sails.log(createdUser.id);
+    return;
+
+  },
+  /**
+   * show detail customer
+   * @param req
+   * @param
+   */
+  show: async function (req, res) {
+    var id = req.param('id');
+
+
+    if (!id) return res.send("No id specified.", 500);
+
+    var customer = await Customer.findOne(id);
+
+    if (!customer) return res.send("User " + id + " not found", 404);
+
+    res.view({
+      customer: customer
+    })
+  }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
   'GET /account/password':   { action: 'account/view-edit-password' },
   'GET /account/profile':    { action: 'account/view-edit-profile' },
   'GET /customer/new' :      { action: 'customer/new' },
-  'GET /customer' :          { action: 'customer'},
+  'GET /customer/show/:id' :     { action: 'customer/show'},
 
 
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
